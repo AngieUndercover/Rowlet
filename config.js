85abb236f2dedc48133ace468ee69e4a0288ffa8
AngieMangie <99761717+AngieMangie@users.noreply.github.com>
2025-01-29 16:04:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -28,5 +28,19 @@ module.exports = {
   "port": 1118,
   "secure": false
     }
+    {
+  "identifier": "ChalresNaig Node",
+  "password": "NAIGLAVA-dash.techbyte.host",
+  "host": "lavahatry4.techbyte.host",
+  "port": 3000,
+  "secure": false
+    }
+    {
+  "identifier": "Public Lavalink v4 NonSSL",
+  "password": "https://dsc.gg/ajidevserver",
+  "host": "lava-v4.ajieblogs.eu.org",
+  "port": 80,
+  "secure": false
+    }
   ]
 }
