9932b4138d43380ec3659bcfe3c3164319f15268
AngieMangie <99761717+AngieMangie@users.noreply.github.com>
2025-01-29 18:08:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-  "identifier": "Public Lavalink v4 NonSSL",
-  "password": "https://dsc.gg/ajidevserver",
-  "host": "lava-v4.ajieblogs.eu.org",
+  "identifier": "Muzykant v4",
+  "password": "https://discord.gg/v6sdrD9kPh",
+  "host": "lavalink_v4.muzykant.xyz",
   "port": 80,
   "secure": false
     }
