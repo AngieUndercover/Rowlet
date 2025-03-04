846bdefcf05cb372bab72c37f0cea2bd461564b0
AngieMangie <99761717+AngieMangie@users.noreply.github.com>
2025-03-03 23:35:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -15,39 +15,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
    {
-  "identifier": "INZEWORLD.COM (DE)",
-  "password": "saher.inzeworld.com",
-  "host": "lava.inzeworld.com",
-  "port": 3128,
-  "secure": false
-    },
-  {
-  "identifier": "ChalresNaig Node",
-  "password": "NAIGLAVA-dash.techbyte.host",
-  "host": "lavahatry4.techbyte.host",
-  "port": 3000,
-  "secure": false
-    },
-    {
   "identifier": "Public Lavalink v4 NonSSL",
   "password": "https://dsc.gg/ajidevserver",
   "host": "lava-v4.ajieblogs.eu.org",
   "port": 80,
   "secure": false
-    },
-    {
-  "identifier": "Muzykant v3",
-  "password": "https://discord.gg/v6sdrD9kPh",
-  "host": "lavalink_v3.muzykant.xyz",
-  "port": 80,
-  "secure": false
-  },
-  {
-  "identifier": "Muzykant v4",
-  "password": "https://discord.gg/v6sdrD9kPh",
-  "host": "lavalink_v4.muzykant.xyz",
-  "port": 80,
-  "secure": false
-  }
+    }
   ]
 }
